ef4a727deab88da357207f3b77fde7ecd8bd2d25
00654434 <livia.lilli@policlinicogemelli.it>
2024-04-18 16:50:01 +0200

    Topic Detection | updated procedures

--- filter_by_topic.py
+++
@@ -1,9 +0,0 @@
-from utils.topic_selector import *
-
-input_path = ''
-output_path = ''
-regex_pattern = "metas|secondar|lesion|nodul|elevata attività metabolica|m\+"
-
-topic_selector_obj = TopicSelector(input_path, output_path, regex_pattern)
-topic_selector_obj.save_topic_data()
-
